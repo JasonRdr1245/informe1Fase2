e0c5ccd4a2324b0c09ede622798c5809835ce1d1
Jason Rodriguez <jasonrch2011@gmail.com>
2023-04-28 00:34:16 -0500

    menu nuevo

--- menu.cpp
+++ menu.cpp
@@ -2,122 +2,159 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
 class Obra
 {
-private:
-  string nombre;
-  string fecha;
 public:
-  Obra (string nombre, string fecha) //constructor
+  std::string nombre;
+  std::string fecha;
+
+public:
+  Obra(const std::string& nombre, const std::string& fecha) : nombre(nombre), fecha(fecha)
   {
-    cout << "Se ha creado la obra " << nombre << " en la fecha de " << fecha << endl;
-    this->nombre = nombre;
-    this->fecha = fecha;
+    std::cout << "Se ha creado la obra " << nombre << " en la fecha de " << fecha << std::endl;
   }
-  ~Obra ()
+
+  ~Obra()
   {
-    cout << "Se ha eliminado la obra " << this->nombre << endl;
+    std::cout << "Se ha eliminado la obra " << nombre << std::endl;
   }
 };
 
 class Artista
 {
 public:
-  string nombre;
+  std::string nombre;
   int edad;
-  vector < Obra * > obrasContainer; //vector de punteros, obras puede existir independientemente del objeto,por eso no se implementa destructor
+  std::vector<Obra*> obrasContainer;
+
 public:
-  Artista& agregarObra (Obra * _obra)
+  Artista(const std::string& nombre, int edad) : nombre(nombre), edad(edad)
   {
-    this->obrasContainer.push_back (_obra);
+    std::cout << "Se ha creado el artista " << nombre << " de edad " << edad << std::endl;
+  }
+
+  ~Artista()
+  {
+    std::cout << "Se ha eliminado el artista " << nombre << std::endl;
+
+    for (auto obra : obrasContainer)
+    {
+      delete obra;
+    }
+  }
+
+  Artista& agregarObra(Obra* obra)
+  {
+    obrasContainer.push_back(obra);
     return *this;
   }
-  Artista& crearObra () //metodo para que un artista cree una obra
-  {
-    string nombre, fecha;
-    cout << "Elija el nombre de la obra: ";
-    cin >> nombre;
-    cout << "Elija la fecha: ";
-    cin >> fecha;
-    agregarObra (new Obra (nombre, fecha));
-    return *this;
-  }
-  Artista (string nombre, int edad) //metodo constructor
-  {
-    cout << "Se ha creado el artista " << nombre << " de edad "<< edad <<  endl;
-    this->nombre = nombre;
-    this->edad = edad;
-  }
-  ~Artista ()
-  {
-    cout << "Se ha eliminado el artista " << this->nombre << endl;
-    for (int i = 0; i < this->obrasContainer.size (); i++)
-      {
-        delete this->obrasContainer[i];
-      }
-  }
+
+  Artista& crearObra()
+ {
+  std::string nombre, fecha;
+  std::cout << "Elija el nombre de la obra: ";
+  std::cin >> nombre;
+  std::cout << "Elija la fecha: ";
+  std::cin >> fecha;
+  Obra* nuevaObra = new Obra(nombre, fecha);
+  agregarObra(nuevaObra);
+  return *this;
+ }
 };
 
-int main ()
+int main()
 {
-  vector < Artista * > artistas;  // Se declara un vector de punteros a objetos Artista
+  std::vector<Artista*> artistas;
   int opcion;
+
   do
-    {
-      cout << " MENU DE OPCIONES " << endl;
-      cout << "1. Agregar artista" << endl;
-      cout << "2. Crear obra" << endl;
-      cout << "3. Salir" << endl;
-      cout << "Elija una opcion: ";
-      cin >> opcion;
-      switch (opcion)
   {
-  case 1:
+    std::cout << " MENU DE OPCIONES"  << std::endl;
+    std::cout << "1. Agregar artista" << std::endl;
+    std::cout << "2. Crear obra" << std::endl;
+    std::cout << "3. Ver obras de un artista" << std::endl;
+    std::cout << "4. Salir del programa" << std::endl;
+    std::cout << "Elija una opcion: ";
+    std::cin >> opcion;
+    switch (opcion)
     {
-      string nombre;
-      int edad;
-      cout << "Ingrese el nombre del artista: ";
-      cin >> nombre;
-      cout << "Ingrese la edad del artista: ";
-      cin >> edad;
-      artistas.push_back (new Artista (nombre, edad)); // El objeto Artista se crea mediante el operador new y 
-      // se pasa el nombre y edad del artista como argumentos para el constructor de la clase Artista.
-      break;
-    }
-  case 2:
-    {
-      int artistaIndex;
-      cout << "Seleccione el artista que va a crear la obra:" << endl; // Se muestra una lista de artistas disponibles
-      for (int i = 0; i < artistas.size (); i++)
-        {
-          cout << i + 1 << ". " << artistas[i]->nombre << endl;
-        }
-      cout << "Elija una opcion: ";
-      cin >> artistaIndex;
-      artistas[artistaIndex - 1]->crearObra ();
-      break;
-    }
-  case 3:
-    {
-      cout << "Fin del Programa" << endl;
-      break;
-    }
-  default:
-    {
-      cout << "Opcion invalida. Intente de nuevo." << endl;
-      break;
-    }
+      case 1:
+      {
+        std::string nombre;
+        int edad;
+        std::cout << "Ingrese el nombre del artista: ";
+        std::cin >> nombre;
+        std::cout << "Ingrese la edad del artista: ";
+        std::cin >> edad;
+        artistas.push_back(new Artista(nombre, edad));
+        break;
+      }
+case 2:
+{
+  int artistaIndex;
+  std::cout << "Seleccione el artista que va a crear la obra:" << std::endl;
+
+  for (int i = 0; i < artistas.size(); i++)
+  {
+    std::cout << i + 1 << ". " << artistas[i]->nombre << std::endl;
   }
-    }
-  while (opcion != 3);
 
-  // Liberar memoria
+  std::cout << "Elija una opcion: ";
+  std::cin >> artistaIndex;
+
+  artistas[artistaIndex - 1]->crearObra();
+  break;
+}
+case 3:
+{
+  int artistaIndex;
+  std::cout << "Seleccione el artista cuyas obras quiere ver:" << std::endl;
+
+  for (int i = 0; i < artistas.size(); i++)
+  {
+    std::cout << i + 1 << ". " << artistas[i]->nombre << std::endl;
+  }
+
+  std::cout << "Elija una opcion: ";
+  std::cin >> artistaIndex;
+
+  // Verificar si el índice del artista es válido
+  if (artistaIndex < 1 || artistaIndex > artistas.size())
+  {
+    std::cout << "Índice de artista inválido." << std::endl;
+    break;
+  }
+
+  Artista *artista = artistas[artistaIndex - 1];
+  std::cout << "Obras de " << artista->nombre << ":" << std::endl;
+
+  for (int i = 0; i < artista->obrasContainer.size(); i++)
+  {
+    std::cout << artista->obrasContainer[i]->nombre << " (" << artista->obrasContainer[i]->fecha << ")" << std::endl;
+  }
+  break;
+}
+case 4:
+{
+  std::cout << "Fin del Programa" << std::endl;
+
+  // Liberar memoria de los artistas y sus obras
   for (int i = 0; i < artistas.size (); i++)
-    {
-      delete artistas[i];
-    }
+  {
+    delete artistas[i];
+  }
+  artistas.clear(); // Limpiar el vector
 
-  return 0;
+  break;
+}
+}
+} while (opcion != 4);
+
+// Liberar memoria
+for (int i = 0; i < artistas.size (); i++)
+{
+delete artistas[i];
+}
+
+return 0;
 }
